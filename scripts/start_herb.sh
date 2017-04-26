9c0ec408b47075780f06905d3a7b99cb08d6b792
Pengju Jin <pengjuj@andrew.cmu.edu>
2017-04-25 20:11:33 -0400

    move unwanted files

--- scripts/start_herb.sh
+++ scripts/start_herb.sh
@@ -8,7 +8,7 @@ function launcher {
         echo "WARNING! $1 is already running!"
     else
         screen -d -S $1 -m bash
-        screen -S $1 -p 0 -X stuff "source /home/herb_home/ros_ws/devel/setup.bash && $2$(printf \\r)"
+        screen -S $1 -p 0 -X stuff "source /home/herb_home/shared/herb0_ws/devel/setup.bash && $2$(printf \\r)"
     fi
 }
 
@@ -27,11 +27,11 @@ launcher    "core"         "roscore"
 sleep 5s
 pr_launcher "state_pub"    "state_publisher.launch"
 pr_launcher "ros_control"  "ros_control.launch"
-# pr_launcher "apriltags"    "apriltags.launch"
-# pr_launcher "joystick"     "joystick.launch"
-# pr_launcher "segway"       "segway.launch"
-# pr_launcher "localization" "localization.launch"
-# pr_launcher "navigation"   "navigation.launch"
-# pr_launcher "talker"       "talker.launch"
+pr_launcher "apriltags"    "apriltags.launch"
+pr_launcher "joystick"     "joystick.launch"
+pr_launcher "segway"       "segway.launch"
+pr_launcher "localization" "localization.launch"
+pr_launcher "navigation"   "navigation.launch"
+pr_launcher "talker"       "talker.launch"
 pr_launcher "diag_logs"    "diagnostic_logging.launch"
-# ssh prdemo@herb2 /home/herb_home/shared/ros_ws/src/herb_launch/scripts/start_herb2.sh
+ssh prdemo@herb2 /home/herb_home/shared/ros_ws/src/herb_launch/scripts/start_herb2.sh
