6e73859e08d723277e2d3ab16c3a2f8f5c2c2de7
Youngsun Kim <yskim041@gmail.com>
2017-11-17 18:36:43 -0800

    clean up start scripts

--- scripts/start_herb2.sh
+++ scripts/start_herb2.sh
@@ -3,11 +3,14 @@
 # to kill all sessions: killall -15 screen
 
 function herb2_launcher {
-    if screen -ls | grep -q $1; then
-        echo "WARNING! $1 is already running!"
+    tmux has-session -t $1 2>/dev/null
+    if [ "$?" -eq 0 ] ; then
+        echo "WARNING! $1 is already running! (tmux session)"
+    elif screen -ls | grep -q $1 ; then
+        echo "WARNING! $1 is already running! (screen session)"
     else
         screen -d -S $1 -m bash
-        screen -S $1 -p 0 -X stuff "source /home/herb_home/shared/herb2_ws/devel/setup.bash; $2$(printf \\r)"
+        screen -S $1 -p 0 -X stuff "source /home/herb_admin/herb2_ws/devel/setup.bash; $2$(printf \\r)"
     fi
 }
 
@@ -15,15 +18,10 @@ function pr_herb2_launcher {
     herb2_launcher $1 "roslaunch herb_launch $2 --wait"
 }
 
-function kinect2_watchdog_launcher {
-    herb2_launcher $1 "python /home/herb_home/shared/herb2_ws/src/kinect_watchdog/kinect_watchdog.py"
-}
+export ROS_MASTER_URI=http://herb0:11311/
+export PYTHONPATH=$PYTHONPATH:/home/herb_admin/tensorflow_models/research:/home/herb_admin/tensorflow_models/research/slim
 
-if [ $(hostname) != 'herb2.personalrobotics.ri.cmu.edu' ]
-then
-    echo 'ERROR: this script must be launched from herb2'
-    exit
-fi
-
-pr_herb2_launcher "kinect2"       "kinect2_bridge.launch"
-kinect2_watchdog_launcher "kinect2_watchdog"
+#pr_herb2_launcher "kinect2"       "kinect2_bridge_cpu.launch"
+pr_herb2_launcher "multisense"    "multisense.launch"
+pr_herb2_launcher "apriltags"	  "apriltags_multisense.launch"
+pr_herb2_launcher "rcnn_moped"    "rcnn_moped.launch"
