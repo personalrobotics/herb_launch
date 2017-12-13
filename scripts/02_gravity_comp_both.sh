9da2f4677a305ad079c5b9b8fa9169377e2c9071
Youngsun Kim <yskim041@gmail.com>
2017-12-12 23:28:42 -0800

    update for demo

--- scripts/02_gravity_comp_both.sh
+++ scripts/02_gravity_comp_both.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 rosrun talker say.py "gravity compensation, left, and right"
-sleep 1s
 rosrun talker say.py "3"
-sleep 1s
 rosrun talker say.py "2"
-sleep 1s
 rosrun talker say.py "1"
-sleep 1s
 rosservice call /controller_manager/switch_controller "start_controllers:
 - 'left_gravity_compensation_controller'
 - 'right_gravity_compensation_controller'
