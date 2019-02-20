6d2740b7bc331595b32a0448a00263cadc39c4f8
Youngsun Kim <yskim041@gmail.com>
2019-02-19 16:08:39 -0800

    Remove talker calls before starting gravcomp.

--- scripts/02_gravity_comp_right.sh
+++ scripts/02_gravity_comp_right.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#run talker say.py "Gravity compensation: right"
-#rosrun talker say.py "3"
-#rosrun talker say.py "2"
-#rosrun talker say.py "1"
 rosservice call /controller_manager/switch_controller "start_controllers:
 - 'right_gravity_compensation_controller'
 strictness: 2"
